17801fa4d74c8afcb8ad903df0b2741899ae5ca2
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-09-17 11:57:15 +0800

    added react-icons dependency, added task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,12 @@
+import Task from './Task';
 
 const Tasks = ({ tasks }) => {
 
     return (
         <div>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>))}
+                <Task key={task.id} task={task}/>
+                ))}
         </div>
     )
 }
