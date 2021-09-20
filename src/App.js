dfe4722745f3a62435d0b627117abd49e9c14b51
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-09-20 09:43:53 +0800

    reminder toggle

--- src/App.js
+++ src/App.js
@@ -30,10 +30,15 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="App">
       <Header title='Task Trackeroo'/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No tasks'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks'}
     </div>
   );
 }
