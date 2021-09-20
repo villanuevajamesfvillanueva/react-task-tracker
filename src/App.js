12dffa993aeff138948b62287418257f769e4fc1
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-09-20 10:19:07 +0800

    added add task functionality

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ function App() {
     },
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -39,7 +47,7 @@ const toggleReminder = (id) => {
   return (
     <div className="App">
       <Header title='Task Trackeroo'/>
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks'}
     </div>
   );
